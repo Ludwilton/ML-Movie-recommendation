d4b078ea304244f3cb837e1b3f01cca51f76e252
Ludwilton <ludwig.carlegrund@gmail.com>
2025-03-26 19:21:22 +0100

    initial commit, various gpt vibe code testing lol

---
+++ recommendation-engine.py
@@ -0,0 +1,17 @@
+
+
+def load_lens_dataset():
+    "loads lens dataset"
+    pass
+
+def load_user_dataset():
+    "loads user dataset"
+    pass
+
+def map_user_data_titles_to_ids():
+    "maps movie id using the name column in user dataset to title in movies.csv"
+    pass
+
+def recommend_movie(user_data):
+    "recommends movie by matching user likeness"
+    pass
